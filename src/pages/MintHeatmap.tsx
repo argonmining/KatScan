3b06272b58251d18d9d8671f76d4ad39830e863c
argonmining <woodashton@outlook.com>
2024-09-30 23:19:10 -0700

    fix API endpoints

--- src/pages/MintHeatmap.tsx
+++ src/pages/MintHeatmap.tsx
@@ -61,7 +61,7 @@ const MintHeatmap: FC = () => {
         try {
             const response = await sendRequest<MintData[]>({
                 method: 'GET',
-                url: 'http://152.53.38.82:3000/api/mint-totals',
+                url: 'https://katapi.nachowyborski.xyz/api/mint-totals',
                 params: {
                     startDate: startDate.toISOString(),
                     endDate: endDate.toISOString()
