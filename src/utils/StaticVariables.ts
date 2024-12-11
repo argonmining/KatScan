3ee4ebf0a26e796bdc86da732be4de24f75e1c95
CrixusHD <nick.engl@gmx.de>
2024-12-11 18:26:16 +0100

    bugfixes-and-improvements-v8
    announcements test

--- src/utils/StaticVariables.ts
+++ src/utils/StaticVariables.ts
@@ -1,6 +1,6 @@
 export const iconBaseUrl = "https://krc20-assets.kas.fyi/icons/"
-export const katscanBaseUrl = process.env.REACT_APP_BASE_URL ?? 'https://katapi.nachowyborski.xyz'
-export const katscanApiUrl = process.env.REACT_APP_BASE_API_URL ?? 'https://katapi.nachowyborski.xyz/api'
+export const katscanBaseUrl = process.env.REACT_APP_BASE_URL ?? 'devkatapi.nachowyborski.xyz'
+export const katscanApiUrl = process.env.REACT_APP_BASE_API_URL ?? 'devkatapi.nachowyborski.xyz/api'
 export const katscanStaticUrl = `${process.env.REACT_APP_BASE_URL ?? 'devkatapi.nachowyborski.xyz'}/static`
 
 export const emptyArray = []
